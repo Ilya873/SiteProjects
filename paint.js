7ec69c14ccca7f1a0a3a1ad56e7d038e243bdd05
Ilya873 <36420509+Ilya873@users.noreply.github.com>
2023-02-26 01:30:49 +0300

    Add files via upload

--- paint.js
+++ paint.js
@@ -5,6 +5,14 @@ const colorPicker = document.getElementById('color-picker');
 const brushSize = document.getElementById('brush-size');
 const opacitySlider = document.getElementById('opacity');
 const saveButton = document.getElementById('save-button');
+const canvasWidthInput = document.getElementById('canvas-width');
+const canvasHeightInput = document.getElementById('canvas-height');
+const resizeButton = document.getElementById('resize-button');
+const clearButton = document.getElementById('clear-button');
+const fillButton = document.getElementById('fill-button');
+const brushButton = document.getElementById('brush-button');
+
+let fillMode = false;
 
 // Сохраняем холст в PNG при нажатии на кнопку
 saveButton.addEventListener('click', function() {
@@ -15,14 +23,30 @@ saveButton.addEventListener('click', function() {
   link.click();
 });
 
-// Устанавливаем размер холста в соответствии с размером экрана
-canvas.width = window.innerWidth * 0.8;
-canvas.height = window.innerHeight * 0.8;
+// Устанавливаем размер холста
+const newWidth = parseInt(canvasWidthInput.value);
+const newHeight = parseInt(canvasHeightInput.value);
+canvas.width = newWidth;
+canvas.height = newHeight;
 
 // Устанавливаем начальный цвет, размер кисти и прозрачность
 let currentColor = colorPicker.value;
 let currentBrushSize = brushSize.value;
 let currentOpacity = opacitySlider.value;
+
+// Отслеживаем изменения размера холста
+resizeButton.addEventListener('click', function() {
+  const newWidth = parseInt(canvasWidthInput.value);
+  const newHeight = parseInt(canvasHeightInput.value);
+  canvas.width = newWidth;
+  canvas.height = newHeight;
+});
+
+// Добавляем обработчик события клика на кнопку
+clearButton.addEventListener('click', function() {
+  // Очищаем холст
+  context.clearRect(0, 0, canvas.width, canvas.height);
+});
 
 // Отслеживаем изменения выбранного цвета
 colorPicker.addEventListener('change', function() {
@@ -39,6 +63,16 @@ opacitySlider.addEventListener('input', function() {
   currentOpacity = opacitySlider.value;
 });
 
+// Нажатие на кнопку "Заливка"
+fillButton.addEventListener('click', function() {
+  fillMode = true;
+});
+
+// Нажатие на кнопку "Кисть"
+brushButton.addEventListener('click', function() {
+  fillMode = false;
+});
+
 // Отслеживаем перемещение мыши на холсте и рисуем линию
 let isDrawing = false;
 let lastX = 0;
@@ -46,8 +80,8 @@ let lastY = 0;
 
 canvas.addEventListener('mousedown', function(e) {
   isDrawing = true;
-  lastX = e.clientX - canvas.offsetLeft;
-  lastY = e.clientY - canvas.offsetTop;
+lastX = e.offsetX;
+lastY = e.offsetY;
   drawLine(e);
 });
 
@@ -61,20 +95,101 @@ canvas.addEventListener('mouseleave', function() {
   isDrawing = false;
 });
 
-// Функция рисования линии
-function drawLine(e) {
-  if (isDrawing === true) {
-    const x = e.clientX - canvas.offsetLeft;
-    const y = e.clientY - canvas.offsetTop;
-    context.strokeStyle = currentColor;
-    context.lineWidth = currentBrushSize;
-    context.globalAlpha = currentOpacity;
-    context.lineCap = 'round';
-    context.beginPath();
-    context.moveTo(lastX, lastY);
-    context.lineTo(x, y);
-    context.stroke();
-    lastX = x;
-    lastY = y;
+function fillCanvas(x, y, fillColor) {
+  // Создаем пустой стек и помещаем в него начальную точку
+  const stack = [[x, y]];
+  
+  // Получаем текущий цвет пикселя в начальной точке
+  const startColor = context.getImageData(x, y, 1, 1).data;
+  
+  // Пока стек не пустой
+  while (stack.length > 0) {
+    // Извлекаем последнюю точку из стека
+    const [x, y] = stack.pop();
+    
+    // Если текущий пиксель не является начальным цветом, пропускаем его
+    const pixelColor = context.getImageData(x, y, 1, 1).data;
+    if (!colorsMatch(pixelColor, startColor)) {
+      continue;
+    }
+    
+    // Иначе закрашиваем текущий пиксель нужным цветом
+    context.fillStyle = fillColor;
+    context.fillRect(x, y, 1, 1);
+    
+    // Добавляем в стек соседние точки
+    if (x > 0) {
+      stack.push([x - 1, y]);
+    }
+    if (x < canvas.width - 1) {
+      stack.push([x + 1, y]);
+    }
+    if (y > 0) {
+      stack.push([x, y - 1]);
+    }
+    if (y < canvas.height - 1) {
+      stack.push([x, y + 1]);
+    }
   }
 }
+
+// Функция для сравнения цветов
+function colorsMatch(color1, color2) {
+  return color1[0] === color2[0]
+    && color1[1] === color2[1]
+    && color1[2] === color2[2]
+    && color1[3] === color2[3];
+}
+
+// Функция рисования линии, закраски
+function drawLine(e) {
+  if (isDrawing === true) {
+    const x = e.offsetX;
+    const y = e.offsetY;
+    if (fillMode === true) {
+      const fillColor = currentColor;
+      fillCanvas(x, y, fillColor);
+    } else {
+      context.strokeStyle = currentColor;
+      context.lineWidth = currentBrushSize;
+      context.globalAlpha = currentOpacity;
+      context.lineCap = 'round';
+      context.beginPath();
+      context.moveTo(lastX, lastY);
+      context.lineTo(x, y);
+      context.stroke();
+      lastX = x;
+      lastY = y;
+    }
+  }
+}
+
+let scaleFactor = 1; // текущий масштаб
+
+canvas.addEventListener('wheel', function(e) {
+  e.preventDefault();
+  
+  // определяем направление скролла
+  let delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+  
+  // вычисляем позицию курсора на холсте
+  const canvasRect = canvas.getBoundingClientRect();
+  const mouseX = (e.clientX - canvasRect.left) / scaleFactor;
+  const mouseY = (e.clientY - canvasRect.top) / scaleFactor;
+  
+  // увеличиваем или уменьшаем масштаб
+  if (delta > 0) {
+    scaleFactor *= 1.1;
+  } else {
+    scaleFactor /= 1.1;
+  }
+  
+  // ограничиваем масштабирование
+  scaleFactor = Math.max(0.1, Math.min(scaleFactor, 10));
+  
+  // устанавливаем новый масштаб холста и точку трансформации
+  const canvasOriginX = mouseX / canvas.width;
+  const canvasOriginY = mouseY / canvas.height;
+  canvas.style.transformOrigin = `${canvasOriginX * 100}% ${canvasOriginY * 100}%`;
+  canvas.style.transform = `scale(${scaleFactor})`;
+});
