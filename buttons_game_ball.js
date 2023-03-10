380e7c292b8b2bc0ddbf5eef4a1b3a9cec467167
Ilya873 <36420509+Ilya873@users.noreply.github.com>
2023-03-11 02:06:07 +0300

    Add files via upload

--- buttons_game_ball.js
+++ buttons_game_ball.js
@@ -61,14 +61,9 @@ if (score_game_record<score_game)
 	let score_save = score_game;
   	score_game = 0;
 	score.innerHTML = score_game;
-	let name_game = prompt(`Количество набранных очков: ${score_save}, введите!!! ваше имя (до 10 символов).`);
+	let name_game = prompt(`Количество набранных очков: ${score_save}, введите ваше имя (до 10 символов).`);
 	name_game = name_game.substring(0, 10);
-firebase.auth().signInAnonymously().then(function() {
-  var user = firebase.auth().currentUser;
-  user.getIdTokenResult().then(function(idTokenResult) {
-    if (idTokenResult.claims.guest) {
-      firebase.database().ref('data').push({
-        	  // Сохраняем имя игрока и количество очков в базу данных
+	  // Сохраняем имя игрока и количество очков в базу данных
   if (name_game) {
     const messagesRef = firebase.database().ref('scores');
     messagesRef.push({
@@ -78,11 +73,6 @@ firebase.auth().signInAnonymously().then(function() {
      // Обновляем таблицу с данными
     updateTable();
   }
-        url: 'https://ilya873.github.io/SiteProjects/game_ball.html'
-      });
-    }
-  });
-});
 });
 
 function updateTable() {
